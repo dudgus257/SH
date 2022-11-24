deb81ed80a6776e8494effd50af026379f4eee75
kimyounghyun <dudgus257@naver.com>
2022-11-24 12:01:03 +0900

    SH

--- admin/inc/dbcon.php
+++ admin/inc/dbcon.php
@@ -1,4 +1,4 @@
 <?php
-$dbcon = mysqli_connect("localhost", "root", "", "front") or die("접속에 실패하였습니다.");
+$dbcon = mysqli_connect("localhost", "dudgus257", "kyh5368young#", "dudgus257") or die("접속에 실패하였습니다.");
 mysqli_set_charset($dbcon, "utf8");
 ?>
